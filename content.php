4647c4d1fd47daf35524f5b653c298bbbbead712
Shashi Gowda <shashigowda91@gmail.com>
2013-09-09 08:38:24 +0530

    Merged.

--- content.php
+++ content.php
@@ -3,10 +3,13 @@
  * @package sesky
  */
 ?>
-<section id="post-<?php the_ID(); ?>" class="post pure-u-1-2">
-    <header class="post-header">
+<section id="post-<?php the_ID(); ?>" class="post pure-u-1-3">
+<?php //the_post_thumbnail( array(300,170) , array('class' => 'post-cover')); ?>
+    <header class="caption">
+		
         <?php sesky_avatar(); ?>
-             <h2 class="post-title pure-u-1"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+         
+             <div class="caption-title"><a class="caption-title" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></div>
 <!--
         <p class="post-meta pure-u-1">
             By <a href="#" class="post-author">Anon</a>
@@ -25,11 +28,12 @@
         </p>
 -->
 <?php sesky_posted_on(); ?>
+
+    <div class="caption-text">
+        <?php //the_content(); ?>
+    </div>
     </header>
 
-    <div class="post-description pure-u-1">
-        <?php the_content(); ?>
-    </div>
 </section>
 <?php /*
 
